9dca7c56139568a9bea2720abb755c3483beb3c4
reverendus <reverendus@invalid>
2018-08-19 15:32:17 +0200

    Manual test file added

---
+++ tests/test_manual.py
@@ -0,0 +1,31 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2017-2018 reverendus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import time
+
+from ethgasstation_client import EthGasStation
+
+
+logging.basicConfig(format='%(asctime)-15s %(levelname)-8s %(message)s', level=logging.DEBUG)
+logging.getLogger('urllib3.connectionpool').setLevel(logging.INFO)
+logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.INFO)
+
+egs = EthGasStation(60, 600)
+while True:
+    print(egs.fast_price())
+    time.sleep(5)
